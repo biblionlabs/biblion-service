3e8b7f8b7fbd91c2b13f660dc90dcb3ce8744cab
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-10-08 18:14:00 -0400

    feat: add common types

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Language {
+    pub id: String,
+    pub english: String,
+    pub local: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BibleVersion {
+    pub id: String,
+    pub name: String,
+    pub language_id: String,
+    pub year: Option<i32>,
+    pub has_nt: bool,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Book {
+    pub id: String,
+    pub name: String,
+    pub order: i32,
+    pub testament: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Verse {
+    pub id: Option<i64>,
+    pub book_id: String,
+    pub chapter: i32,
+    pub verse: i32,
+    pub text: String,
+    pub bible_version_id: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CrossReference {
+    pub id: Option<i64>,
+    pub from_verse_id: i64,
+    pub to_verse_id: i64,
 }
